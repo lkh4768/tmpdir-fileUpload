fe2b68802a8b313c999d6e320d59fe80aeb449a0
lkh4768 <lkh5510@gmail.com>
2018-03-04 14:09:10 +0900

    update jenkinsfile

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -54,12 +54,9 @@ node {
 
 			stage "Performance testing"
 				STAGE = "Performance testing"
-        sh "ls -al /var/jenkins_home/"
-        sh "ls -al /var/jenkins_home/thrid-party/"
-        sh "ls -al /var/jenkins_home/thrid-party/apache-jmeter-4.0/"
         sh "ls -al /var/jenkins_home/thrid-party/apache-jmeter-4.0/bin/"
 
-        sh "mkdir -p ./build/jmeter && java -jar /var/jenkins_home/thrid-party/apache-jmeter-4.0/bin/ApacheJMeter.jar -n -t ./performanc_test.jmx -l ./build/jmeter/preformance_test_report.jtl"
+        sh "mkdir -p ./build/jmeter && java -jar /var/jenkins_home/thrid-party/apache-jmeter-4.0/bin/ApacheJMeter.jar -n -t ./script/jmeter/performanc_test.jmx -l ./build/jmeter/preformance_test_report.jtl"
         step([$class: 'ArtifactArchiver', artifacts: './build/jmeter/performance_test_report.jtl'])
 		}
 
