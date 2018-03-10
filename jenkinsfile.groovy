e2033a46f9efab4cdd485a0f21a5e8b781ec172c
lkh4768 <lkh5510@gmail.com>
2018-03-10 16:41:12 +0900

    update jenkinsfile - update jmeter script

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -57,7 +57,7 @@ node {
         sh "ls -al /var/jenkins_home/thrid-party/apache-jmeter-4.0/bin/"
         sh "mkdir -p ./build/jmeter && /var/jenkins_home/thrid-party/apache-jmeter-4.0/bin/jmeter -n -t config/jmeter/performance_test.jmx -p config/jmeter/tmpdir.properties -l build/jmeter/preformance_test_report.jtl"
         sh "ls -al ./build/jmeter/"
-        step([$class: 'ArtifactArchiver', artifacts: './build/jmeter/*.jtl'])
+        step([$class: 'ArtifactArchiver', artifacts: 'build/jmeter/*.jtl'])
 		}
 
 		if(env.BRANCH_NAME == MASTER_BRANCH){
