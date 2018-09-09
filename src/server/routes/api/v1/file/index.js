9f378fecbc733247935c5d239bafab1114bf8649
이기현 <lkh5510@gmail.com>
2018-09-09 16:28:36 +0000

    Adding file upload feature
    1. Add multer package for file upload and store
    2. Add uuid package for when file save, it don't overlay
    
    Resolving #7

--- src/server/routes/api/v1/file/index.js
+++ src/server/routes/api/v1/file/index.js
@@ -1,8 +1,18 @@
 import express from 'express';
+import uuidv1 from 'uuid/v1';
+import multer from 'multer';
+import Config from 'config';
 
 const router = express.Router();
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    console.log(Config.get('tmpdir.file.root'));
+    cb(null, Config.get('tmpdir.file.root'));
+  }
+});
+const upload = multer({ storage });
 
-router.post('/', (req, res) => {
+router.post('/', upload.array(), (req, res) => {
   const currentDate = (new Date()).getTime();
   res.json({ id: '1', submissionTime: currentDate, expireTime: currentDate+1 });
 });
--- src/server/routes/api/v1/file/index.test.js
+++ src/server/routes/api/v1/file/index.test.js
@@ -1,5 +1,6 @@
 import request from 'supertest';
 import Config from 'config';
+import rimraf from 'rimraf';
 import app, { server } from '../../../../index.js';
 
 afterAll((done) => {
@@ -8,16 +9,26 @@ afterAll((done) => {
 
 describe('file', () => {
   test('[router] POST /api/v1/file, Success', (done) => {
+    const fileRootPath = Config.get('tmpdir.file.root');
+    rimraf.sync(`${fileRootPath}/*`);
     const beforeSendTime = (new Date()).getTime();
     request(app)
       .post('/api/v1/file')
-      .send('a')
+      .field('name', 'file0')
+      .attach('file0', '/workspace/tmpdir-fileUpload/data/test/mb.txt')
       .expect(200)
       .end((err, res) => {
         if(err) {
           return done(err);
         }
-
+      
+        // check file
+        const listInRoot = fs.readdirSync(fileRootPath);
+        console.log(listInRoot);
+        expect(listInRoot.length).toEqual(1);
+        expect(listInRoot.length).toEqual(1);
+      
+        // check fileInfo
         const afterSendTime = (new Date()).getTime();
         expect(typeof res.body.id === 'string').toEqual(true);
         expect(res.body.submissionTime).toBeGreaterThanOrEqual(beforeSendTime);
