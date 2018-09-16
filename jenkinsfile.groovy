f94beeed035afedd2ee834f2cb126cf0848495dd
lkh4768 <lkh5510@gmail.com>
2018-09-16 20:57:19 +0900

    Update jenkinsfile.groovy
    Fixing remove script stdout newline
    
    Resolving #18

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -7,9 +7,9 @@ node {
       checkout scm
       DEVELOP_BRANCH = "develop"
       MASTER_BRANCH = "master"
-      PACKAGE_NAME_LOW = sh script: "cat package.json | grep name | head -1 | awk -F: '{ print \$2 }' | sed 's/[\",]//g' | sed 's/ //g' | sed -e 's/./\\L\\0/g'", returnStdout: true
-      PACKAGE_VERSION = sh script: "cat package.json | grep version | head -1 | awk -F: '{ print \$2 }' | sed 's/ //g' | sed 's/[\",]//g'", returnStdout: true
-      IMAGE_NAME = "tmpdir-${PACKAGE_NAME_LOW}-${PACKAGE_VERSION}"
+      PACKAGE_NAME_LOW = sh (script: "cat package.json | grep name | head -1 | awk -F: '{ print \$2 }' | sed 's/[\",]//g' | sed 's/ //g' | sed -e 's/./\\L\\0/g'", returnStdout: true).trim()
+      PACKAGE_VERSION = sh (script: "cat package.json | grep version | head -1 | awk -F: '{ print \$2 }' | sed 's/ //g' | sed 's/[\",]//g'", returnStdout: true).trim()
+      IMAGE_NAME = "${PACKAGE_NAME_LOW}-${PACKAGE_VERSION}"
       REGISTRY_HOST = "dev.sw-warehouse.xyz:1450"
       REGISTRY_USER = "root"
       REGISTRY_PASSWORD = "10WESfpwltmxmfl"
